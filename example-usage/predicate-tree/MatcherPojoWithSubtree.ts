a9ce2e9943062425472e8efeb4f4dff272c3bef4
terary <terary@gmail.com>
2022-12-22 07:25:15 -0600

    stash

--- example-usage/predicate-tree/MatcherPojoWithSubtree.ts
+++ example-usage/predicate-tree/MatcherPojoWithSubtree.ts
@@ -101,6 +101,62 @@ const addressTreePojo = {
   },
 };
 
+const addressTreePojo = {
+  "customer.address": {
+    nodeType: "subtree",
+    parentId: "customer.address",
+    nodeContent: { operator: "$addressTree" },
+  },
+  "customer.address.address1": {
+    parentId: "customer.address",
+    nodeContent: { operator: "$eq", subjectId: "customer.address.address1", value: "addr1" },
+  },
+  "customer.address.address2": {
+    parentId: "customer.address",
+    nodeContent: { operator: "$eq", subjectId: "customer.address.address2", value: "addr2" },
+  },
+  "customer.address.address3": {
+    parentId: "customer.address",
+    nodeContent: { operator: "$eq", subjectId: "customer.address.address3", value: "addr3" },
+  },
+  "customer.address.city": {
+    parentId: "customer.address",
+    nodeContent: { operator: "$eq", subjectId: "customer.address.city", value: "city" },
+  },
+  "customer.address.stateOrProvince": {
+    parentId: "customer.address",
+    nodeContent: {
+      operator: "$eq",
+      subjectId: "customer.address.stateOrProvince",
+      value: "stateOrProvince",
+    },
+  },
+  "customer.address.postalCode": {
+    parentId: "customer.address",
+    nodeContent: {
+      operator: "$eq",
+      subjectId: "customer.address.postalCode",
+      value: "postalCode",
+    },
+  },
+  "customer.address.country": {
+    parentId: "customer.address",
+    nodeContent: {
+      operator: "$eq",
+      subjectId: "customer.address.country",
+      value: "country",
+    },
+  },
+  "customer.address.specialInstructions": {
+    parentId: "customer.address",
+    nodeContent: {
+      operator: "$eq",
+      subjectId: "customer.address.specialInstructions",
+      value: "specialInstructions",
+    },
+  },
+};
+
 const matcherPojo = {
   ...rootPojo,
   ...rubblePojo,
@@ -108,7 +164,11 @@ const matcherPojo = {
 } as TTreePojo<TPredicateNodeTypes>;
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 export { matcherPojo };
+=======
+export { matcherPojo, notTree as notTreePojo, agePojo, addressTreePojo };
+>>>>>>> Stashed changes
 =======
 export { matcherPojo, notTree as notTreePojo, agePojo, addressTreePojo };
 >>>>>>> Stashed changes
